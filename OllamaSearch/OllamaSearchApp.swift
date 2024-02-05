0e026eab99d66cb1d1727a6727d3a44315c4686e
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2024-02-05 19:46:16 +0300

    Initial Commit

---
+++ OllamaSearch/OllamaSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  OllamaSearchApp.swift
+//  OllamaSearch
+//
+//  Created by Олег Рыбалко on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct OllamaSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
