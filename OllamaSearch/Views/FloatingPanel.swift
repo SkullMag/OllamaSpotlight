8f8514e5107cdd6dd867a4850580f6cae6bf0160
Oleg Rybalko <rybalko.oleg.123@mail.ru>
2024-02-10 22:40:13 +0300

    removed redundant self

--- OllamaSearch/Views/FloatingPanel.swift
+++ OllamaSearch/Views/FloatingPanel.swift
@@ -16,29 +16,29 @@ class FloatingPanel: NSPanel {
 //        self.setFrameAutosaveName("ollama search")
 
         // Allow the pannel to be on top of almost all other windows
-        self.isFloatingPanel = true
-        self.level = .floating
+        isFloatingPanel = true
+        level = .floating
 
         // Allow the pannel to appear in a fullscreen space
-        self.collectionBehavior.insert(.fullScreenAuxiliary)
+        collectionBehavior.insert(.fullScreenAuxiliary)
 
         // While we may set a title for the window, don't show it
-        self.titleVisibility = .hidden
-        self.titlebarAppearsTransparent = true
+        titleVisibility = .hidden
+        titlebarAppearsTransparent = true
 
         // Since there is no titlebar make the window moveable by click-dragging on the background
-        self.isMovableByWindowBackground = true
+        isMovableByWindowBackground = true
 
         // Keep the panel around after closing since I expect the user to open/close it often
-        self.isReleasedWhenClosed = false
+        isReleasedWhenClosed = false
 
         // Activate this if you want the window to hide once it is no longer focused
-        self.hidesOnDeactivate = true
+        hidesOnDeactivate = true
 
         // Hide the traffic icons (standard close, minimize, maximize buttons)
-        self.standardWindowButton(.closeButton)?.isHidden = true
-        self.standardWindowButton(.miniaturizeButton)?.isHidden = true
-        self.standardWindowButton(.zoomButton)?.isHidden = true
+        standardWindowButton(.closeButton)?.isHidden = true
+        standardWindowButton(.miniaturizeButton)?.isHidden = true
+        standardWindowButton(.zoomButton)?.isHidden = true
     }
 
     // `canBecomeKey` and `canBecomeMain` are required so that text inputs inside the panel can receive focus
